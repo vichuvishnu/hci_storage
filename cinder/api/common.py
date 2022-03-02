34d52a1ddb38c34843ea8012122d82a27f70a572
Vishnu Bhaskar <41546654+vichuvishnu@users.noreply.github.com>
2022-03-02 15:57:24 +0530

    Update common.py
    command added

--- cinder/api/common.py
+++ cinder/api/common.py
@@ -1,5 +1,6 @@
 # Copyright 2010 OpenStack Foundation
 # All Rights Reserved.
+# Acceleron Labs vishnu
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
